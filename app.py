4df9da4e177b9ff98ee6a8d025004815362b3f77
Marcelo Rissi <marcelorissi2@gmail.com>
2023-02-16 11:17:09 -0300

    Criado arquivo notebook de teste

--- app.py
+++ app.py
@@ -40,21 +40,13 @@ def create_answers_section(df):
 
     st.header("First Round")
     st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
-
     st.subheader("How many bikes are being sold are bikes from a unique owner?")
-
     st.subheader("Are high kilometer bikes more expensive than bikes with lower kilometer?")
-
     st.subheader("Are the bikes with a unique owner more expense on avarege than the other bikes?")
-
     st.subheader("Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?")
-
     st.subheader("Which company has the most bikes registered?")
-
-    st.subheader("Which company has the most expensive bikes on avarege?")
-
+    st.subheader("Wich company has the most expensive bikes on averege")
     st.subheader("Are the company that has the most expensive bikes registered also the company with the most bikes registered?")
-
     st.subheader("Which bikes are good for buying?")
     
     return None
